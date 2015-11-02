e2fd007de51cf8b7a7c078249ac5e27d7391cb3a
Rasmus Pind <rasmus.pind@khk.ee>
2015-11-02 12:38:45 +0200

    1. Tabeli sortimine {Delivers #107186518}

---
+++ yl_1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<script type="text/javascript" src="sorttable.js"></script>
+
+<table class="sortable" style="width:30%">
+    <h1>Olümpiavõitjad</h1>
+
+    <thead>
+    <tr>
+        <th>Aasta</th>
+        <th>Koht</th>
+        <th>Nimi</th>
+        <th>Spordiala</th>
+        <th>Tulemus</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr>
+        <td>2008</td>
+        <td>Peking</td>
+        <td>Gerd Kanter</td>
+        <td>Kettaheide</td>
+        <td>68.82</td>
+    </tr>
+    <tr>
+        <td>1980</td>
+        <td>Moskva</td>
+        <td>Jaak Uudmäe</td>
+        <td>Kolmikhüpe</td>
+        <td>17.35</td>
+    </tr>
+
+    <tr>
+        <td>1972</td>
+        <td>München</td>
+        <td>Jüri Tarmak</td>
+        <td>Kõrgushüpe</td>
+        <td>2.23</td>
+    </tr>
+
+    <tr>
+        <td>1964</td>
+        <td>Innsbruck</td>
+        <td>Ants Antson</td>
+        <td>Kiiruisutamine</td>
+        <td>2.10,3</td>
+    </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
